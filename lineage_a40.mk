01c621ce82c844f0d9d64bb301008976b7c818e4
kevios12 <kerschbaumer.kevin2@gmail.com>
2021-05-20 11:33:11 +0200

    Update lineage_a40.mk

--- lineage_a40.mk
+++ lineage_a40.mk
@@ -1,14 +1,14 @@
-# Copyright (C) 2018 The LineageOS Project
-# SPDX-License-Identifier: Apache-2.0
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/a40/device.mk)
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from a40 device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a40
@@ -18,6 +18,7 @@ PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+TARGET_VENDOR := samsung
 TARGET_VENDOR_PRODUCT_NAME := a40dd
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
